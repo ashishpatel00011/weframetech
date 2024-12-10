364ade1e0e09c1658b01380add41ac152972dc57
Ashishpatel011 <iamashish761@gmail.com>
2024-12-10 09:17:03 +0530

    header update

--- app/Components/header.tsx
+++ app/Components/header.tsx
@@ -1,27 +1,28 @@
-"use client";
+'use client';
 
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import Image from 'next/image'; // Optimized image handling
-import logo from './logo.png'; // Adjust the path as per your folder structure
-import SearchIcon from '@mui/icons-material/Search';
+import { useState, useEffect, Fragment } from 'react';
+import Image from 'next/image';
+import { Dialog, Transition } from '@headlessui/react';
+import logo from './logo.png'; // Adjust the path
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import MenuIcon from '@mui/icons-material/Menu'; // Import the hamburger menu icon
-import CloseIcon from '@mui/icons-material/Close'; // Close icon for the mobile menu
+import MenuIcon from '@mui/icons-material/Menu'; // Hamburger menu icon
+import CloseIcon from '@mui/icons-material/Close'; // Close icon
+import Search, { SearchSkeleton } from './maincontent/search';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+    const openMobileMenu = () => setIsMenuOpen(true);
+    const closeMobileMenu = () => setIsMenuOpen(false);
 
-    // Close the menu when the screen size changes
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth >= 640) {
-                setIsMenuOpen(false); // Close menu on larger screens
+            if (window.innerWidth >= 660) {
+                setIsMenuOpen(false);
             }
         };
         window.addEventListener('resize', handleResize);
@@ -30,41 +31,24 @@ const Header = () => {
 
     return (
         <div className="header">
-            {/* Top Header with shadow for small screens */}
-            <header className="flex items-center justify-between md:px-4 md:py-1">
-                {/* Hamburger Menu for Mobile */}
-                <div className="sm:hidden flex items-center">
-                    <button onClick={toggleMenu}>
-                        {isMenuOpen ? (
-                            <CloseIcon className="text-gray-700" />
-                        ) : (
-                            <MenuIcon className="text-gray-700" />
-                        )}
-                    </button>
-                </div>
-                {/* Left Side - Logo */}
+            {/* Top Header */}
+            <header className="flex items-center justify-between px-4 py-1">
+                {/* Hamburger Menu */}
+                <button
+                    onClick={openMobileMenu}
+                    aria-label="Open mobile menu"
+                    className="sm:hidden flex items-center"
+                >
+                    <MenuIcon className="text-gray-700" />
+                </button>
+                {/* Logo */}
                 <div className="flex items-center space-x-4">
-                    <Image
-                        src={logo} // Update the path based on your logo's location
-                        alt="WeFrameTech Logo"
-                        width={100}
-                        height={70}
-                        className="object-contain"
-                    />
+                    <Image src={logo} alt="WeFrameTech Logo" width={100} height={70} className="object-contain" />
                 </div>
-
-                {/* Search Bar */}
-                <div className="flex-grow max-w-2xl mx-8 relative pl-0 hidden sm:block">
-                    <input
-                        type="text"
-                        placeholder="Rechercher"
-                        className="w-full px-4 py-2 text-sm border rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-teal-500"
-                    />
-                    <SearchIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                </div>
-
+                {/* Search */}
+                <Search />
                 {/* Right Side - Navigation for Large Screens */}
-                <div className="flex lg:items-center space-x-1 md:space-x-8 md:text-xs">
+                <div className="flex items-center space-x-2 md:space-x-8 md:text-xs ml-16">
                     <Link href="#" className="flex items-center text-gray-700 text-sm hover:text-teal-500">
                         <LightbulbIcon className="text-gray-700" />
                         <p className='hidden lg:block'>Inspirations</p>
@@ -77,68 +61,93 @@ const Header = () => {
                         <ShoppingCartIcon />
                         <p className='hidden lg:block'>Panier</p>
                     </Link>
-                    <Link href="#" className="flex items-center space-x-2">
-                        <div className="w-8 h-8 rounded-full bg-gray-300 hidden lg:block" ></div>
-                        <span className="text-gray-700 text-sm ">FR</span>
+                    <Link href="#" className="md:flex items-center space-x-2 hidden">
+                        <div className="w-8 h-8 rounded-full bg-gray-300"></div>
+                        <span className="text-gray-700 text-sm hidden lg:block">FR</span>
                         <ArrowDropDownIcon className="text-gray-700 hidden lg:block" />
                     </Link>
                 </div>
             </header>
-            <div className=" relative mt-6 md:hidden z-0">
-                    <input
-                        type="text"
-                        placeholder="Rechercher"
-                        className="w-full px-4 py-2 text-sm border rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-teal-500"
-                    />
-                    <SearchIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                </div>
-            {/* Mobile Menu (will appear when isMenuOpen is true) */}
-            {isMenuOpen && (
-                <div className="sm:hidden bg-white shadow-md p-4 z-50">
-                    <nav className="flex flex-col space-y-2">
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            ART DE LA TABLE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            MOBILIER
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            NAPPAGE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            MATÉRIEL DE SALLE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            CUISINE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            BARBECUE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            TENTE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            CHAUFFAGE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            PODIUM-PISTE DE DANSE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            SON ET LUMIÈRE
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            PACKS
-                        </Link>
-                        <Link href="#" className="text-gray-700 hover:text-teal-500">
-                            CONSOMMABLES
-                        </Link>
-                    </nav>
-                </div>
-            )}
+            <SearchSkeleton />
+            {/* Mobile Menu using Dialog */}
+            <Transition show={isMenuOpen} as={Fragment}>
+                <Dialog onClose={closeMobileMenu} className="relative z-50">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="transition-opacity duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition-opacity duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/30" />
+                    </Transition.Child>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="transition-transform duration-300"
+                        enterFrom="-translate-x-full"
+                        enterTo="translate-x-0"
+                        leave="transition-transform duration-200"
+                        leaveFrom="translate-x-0"
+                        leaveTo="-translate-x-full"
+                    >
+                        <Dialog.Panel className="fixed inset-0 bg-white dark:bg-black p-4 w-1/2">
+                            <button
+                                onClick={closeMobileMenu}
+                                aria-label="Close mobile menu"
+                                className="flex items-center justify-center mb-4"
+                            >
+                                <CloseIcon className="text-gray-700" />
+                            </button>
+                            <div className="sm:hidden  p-4">
+                                <nav className="flex flex-col space-y-3 ">
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        ART DE LA TABLE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        MOBILIER
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        NAPPAGE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        MATÉRIEL DE SALLE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        CUISINE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        BARBECUE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        TENTE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        CHAUFFAGE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        PODIUM-PISTE DE DANSE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        SON ET LUMIÈRE
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        PACKS
+                                    </Link>
+                                    <Link href="#" className="text-gray-700 hover:text-teal-500">
+                                        CONSOMMABLES
+                                    </Link>
+                                </nav>
+                            </div>
+                        </Dialog.Panel>
+                    </Transition.Child>
+                </Dialog>
+            </Transition>
 
             {/* Bottom Header (Always Visible) */}
-            <div className="hidden sm:flex items-center justify-between px-4 py-2 border-b-2 border-gray-200">
-                <div className="relative w-full overflow-x-auto scrollbar-hide">
+            <div className="hidden sm:flex items-center justify-around px-4 py-2 border-b-2 border-gray-200">
+                <div className="relative w-full overflow-x-auto xl:scrollbar-hide">
                     {/* Left and Right arrows for slider */}
                     <div className="flex space-x-8 py-2 text-xs whitespace-nowrap">
                         <Link href="#" className="text-gray-700 hover:text-teal-500 cursor-pointer">
---
+++ app/Components/maincontent/search.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import SearchIcon from '@mui/icons-material/Search';
+import Form from 'next/form';
+import { useSearchParams } from 'next/navigation';
+
+export default function Search() {
+  const searchParams = useSearchParams();
+  
+  return (
+    <Form action="#" className="flex-grow max-w-2xl mx-8 relative pl-0 hidden md:block">
+      <input
+        key={searchParams?.get('q')}
+        type="text"
+        name="q"
+        placeholder="Rechercher de produits..."
+        autoComplete="off"
+        defaultValue={searchParams?.get('q') || ''}
+        className="text-md w-full rounded-lg border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
+      />
+      <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
+      <SearchIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+      </div>
+    </Form>
+  );
+}
+
+export function SearchSkeleton() {
+  return (
+    <form className="flex-grow relative pl-0 block w-full md:hidden my-4">
+      <input
+        placeholder="Rechercher de produits..."
+        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
+      />
+      <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
+      <SearchIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+      </div>
+    </form>
+  );
+}
